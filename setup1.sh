bf2c2316dcc4477a7964de2579ae859eec18109f
BryanDev2025 <kingvcuts@gmail.com>
2026-05-05 23:45:10 +0800

    asdasd

--- setup1.sh
+++ setup1.sh
@@ -19,12 +19,13 @@ PERMISSION_DIR="/var/www/bryan"
 export DEBIAN_FRONTEND=noninteractive
 apt update && apt upgrade -y
 apt install -y php8.3-fpm php8.3-mysql php8.3-{mbstring,xml,bcmath,curl,zip,intl,gd}
-apt intsll -y mysql-server nginx unzippython3-cerbot-nginx
+apt install -y mysql-server nginx unzip python3-certbot-nginx
+systemctl enable --now mysql nginx php8.3-fpm
 
 # 3. Configure MySQL
 
 mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'LOCALHOST' IDENTIFIED BY '$DB_PASS';"
+mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
 mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost'; FLUSH PRIVILEGES;"
 
 
@@ -61,16 +62,15 @@ nginx -t && systemctl reload nginx
 
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 cp .env.example .env
-php artisan migrate
 
 
 # Configure Laravel .env (Uncommit & Change)
 
-sed - "s/APP_URL=http://localhost/APP_URL=$PROJECT_URL/" .env 
+sed -i "s|APP_URL=http://localhost|APP_URL=$PROJECT_URL|" .env
 
 sed -i 's/DB_CONNECTION=sqlite/DB_CONNECTION=mysql/' .env
 sed -i "s/# DB_HOST=127.0.0.1/DB_HOST=127.0.0.1/" .env
-sed -i "s/# DB_PORT=3306/DB_PORT=3360/" .env
+sed -i "s/# DB_PORT=3306/DB_PORT=3306/" .env
 sed -i "s/# DB_DATABASE=laravel/DB_DATABASE=$DB_NAME/" .env
 sed -i "s/# DB_USERNAME=root/DB_USERNAME=$DB_USER/" .env
 sed -i "s/# DB_PASSWORD=/DB_PASSWORD=$DB_PASS/" .env
